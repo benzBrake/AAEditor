69b7a13c88e04d4b9594d996d5f16381c47394a0
Ryan Lieu <github-benzBrake@woai.ru>
2024-06-25 11:39:05 +0800

    fix: 链接匹配错误

--- assets/src/editor-js.php
+++ assets/src/editor-js.php
@@ -97,12 +97,12 @@
     }, {
         id: 'wmd-center-button',
         name: "<?php _e("居中 <center>") ?>",
-        icon:'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="20" height="20"><path d="M11 4V2H13V4H19C19.5523 4 20 4.44772 20 5V10C20 10.5523 19.5523 11 19 11H13V13H17C17.5523 13 18 13.4477 18 14V19C18 19.5523 17.5523 20 17 20H13V22H11V20H7C6.44772 20 6 19.5523 6 19V14C6 13.4477 6.44772 13 7 13H11V11H5C4.44772 11 4 10.5523 4 10V5C4 4.44772 4.44772 4 5 4H11ZM8 15V18H16V15H8ZM6 9H18V6H6V9Z"></path></svg>',
+        icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="20" height="20"><path d="M11 4V2H13V4H19C19.5523 4 20 4.44772 20 5V10C20 10.5523 19.5523 11 19 11H13V13H17C17.5523 13 18 13.4477 18 14V19C18 19.5523 17.5523 20 17 20H13V22H11V20H7C6.44772 20 6 19.5523 6 19V14C6 13.4477 6.44772 13 7 13H11V11H5C4.44772 11 4 10.5523 4 10V5C4 4.44772 4.44772 4 5 4H11ZM8 15V18H16V15H8ZM6 9H18V6H6V9Z"></path></svg>',
         insertBefore: '#wmd-spacer1',
         command() {
-            this.wrapText(`<center>`,`</center>`);
+            this.wrapText(`<center>`, `</center>`);
         }
-    },{
+    }, {
         id: 'wmd-link-button',
         remove: true
     }, {
@@ -112,7 +112,7 @@
         shortcut: 'ctrl+l',
         insertBefore: '#wmd-spacer2',
         command() {
-            const markdownReg = /\[(?!.*?\]\().*?\]\((.*?)\)/ig; // Markdown 链接正则
+            const markdownReg = /\[([^\]]+)\]\(([^)]+)\)/ig; // Markdown 链接正则
             const httpLinkReg = /https?:\/\/\S+/ig; // HTTP链接正则
             const {textarea} = this;
             let lastSelection = textarea.getSelection();
