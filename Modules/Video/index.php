8e469ef1df8ae5bff1b255eb3803d919a2ae7090
Ryan Lieu <github-benzBrake@woai.ru>
2024-08-22 10:15:48 +0800

    fix: 修复上个版本无法在编辑时预览视频的问题

--- Modules/Video/index.php
+++ Modules/Video/index.php
@@ -23,16 +23,17 @@ class ModuleVideo implements Module
         <script>
             $('body').trigger('XEditorAddButton', [{
                 id: 'wmd-video-button',
-                name: '<?php _e("引用m3u8/mp4"); ?>',
+                name: '<?php _e("引用m3u8/mp4/Youtube"); ?>',
                 icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"><path d="M3 3.9934C3 3.44476 3.44495 3 3.9934 3H20.0066C20.5552 3 21 3.44495 21 3.9934V20.0066C21 20.5552 20.5551 21 20.0066 21H3.9934C3.44476 21 3 20.5551 3 20.0066V3.9934ZM5 5V19H19V5H5ZM10.6219 8.41459L15.5008 11.6672C15.6846 11.7897 15.7343 12.0381 15.6117 12.2219C15.5824 12.2658 15.5447 12.3035 15.5008 12.3328L10.6219 15.5854C10.4381 15.708 10.1897 15.6583 10.0672 15.4745C10.0234 15.4088 10 15.3316 10 15.2526V8.74741C10 8.52649 10.1791 8.34741 10.4 8.34741C10.479 8.34741 10.5562 8.37078 10.6219 8.41459Z"></path></svg>',
                 insertAfter: '#wmd-audio-album-button|#wmd-spacer3',
                 command() {
                     this.openModal({
-                        title: '<?php _e("引用m3u8/mp4"); ?>',
+                        title: '<?php _e("引用m3u8/mp4/Youtube"); ?>',
                         innerHTML: `<div class="form-item">
     <label for="src" class="required"><?php _e("视频链接") ?></label>
     <input type="text" required name="src">
 </div>
+<div style="color: #666">视频链接额外支持填入 Youtube 链接，eg:<br>https://www.youtube.com/watch?v=LqfimuFAFJ8</div>
 <div class="form-item">
     <label for="autoplay"><?php _e("自动播放") ?></label>
     <select name="autoplay">
@@ -114,7 +115,7 @@ class ModuleVideo implements Module
                                 if (named.src) {
                                     let autoplay = (named.autoplay ?? "off") === 'on';
                                     let player = named.player || "<?php echo self::defaultPlayer() ?>";
-                                    return `<iframe src="${player}?src=${encodeURIComponent(named.src)}&autoplay=${autoplay}" frameborder="0" allowfullscreen></iframe>`;
+                                    return `<iframe src="${player}?url=${encodeURIComponent(named.src)}&autoplay=${autoplay}" frameborder="0" allowfullscreen style="width: 100%; aspect-ratio: 16 / 9"></iframe>`;
                                 } else {
                                     return '';
                                 }
@@ -133,8 +134,8 @@ class ModuleVideo implements Module
                                 let {named} = t.parseShortCodeAttrs(arguments[3]);
                                 if (named.id) {
                                     let autoplay = named.autoplay ?? "off";
-                                    let src = "https://www.bilibili.com/blackboard/html5mobileplayer.html?" + (`${name.id}`.startsWith('BV') ? 'bvid' : 'aid') + `=${named.id}` + `&page=${named.page ?? 1}` + "&fjw=" + (autoplay === "on" ? 'true' : 'false');
-                                    return `<div class="x-bilibili-wrapper"><iframe src="${src}" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" /></div>`;
+                                    let src = "https://www.bilibili.com/blackboard/html5mobileplayer.html?" + (named.id.startsWith('BV') ? 'bvid' : 'aid') + `=${named.id}` + `&page=${named.page ?? 1}` + "&fjw=" + (autoplay === "on" ? 'true' : 'false');
+                                    return `<div class="x-video bilibili"><iframe src="${src}" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" /></div>`;
                                 } else {
                                     return '<div class="x-alert" type="warning"><span class="x-alert-icon"></span><div class="x-alert-content"><?php _e("Bvid 未填写") ?></div></div>';
                                 }
@@ -213,7 +214,7 @@ class ModuleVideo implements Module
                     $page = $attrs['page'] ?? '1';
                     $autoplay = ($attrs['autoplay'] ?? 'off') === 'on';
                     $src = "//www.bilibili.com/blackboard/html5mobileplayer.html?" . $idType . '=' . $vid . '&page=' . $page . '&fjw=' . $autoplay;
-                    return '<div class="x-bilibili-wrapper"><iframe class="x-bilibili" src="' . $src . '" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe></div>';
+                    return '<div class="x-video bilibili"><iframe src="' . $src . '" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe></div>';
                 }
                 return '<div class="x-alert" type="warning"><span class="x-alert-icon"></span><div class="x-alert-content">' . _t("Bvid 未填写") . '</div></div>';
             }, $text);
